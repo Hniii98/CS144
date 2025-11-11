0df143240e61e1bfba3b13f3381e44f3d6e02791
hniii98 <752608928@qq.com>
2025-11-11 16:33:10 +0900

    fix: fix some naming to make it clear

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -12,25 +12,25 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   /*
-   * raw_data_ =  zero_point + n mod 2^32
+   * raw_data_ =  zero_point + n mod 2^32 = zero_point + remainder
    * n = (zero_point - raw_data_) + k * 2^32 = offset + k * 2^32
    * Since we need to find the corresponding absolute sequnence number that is closest to checkponit,
    * so consider base of checkpoint as rough base. When the absolute sequence number is in the same base,
    * rough base is the real base. Otherwise check the nearby by shifting rough base.
    */
-  uint64_t offset = raw_value_ - zero_point.raw_value_;
-  uint64_t rough_base = checkpoint & ~0xFFFFFFFFULL;
-  uint64_t candidate = rough_base + offset;
+  uint64_t remainder = static_cast<uint64_t>(raw_value_ - zero_point.raw_value_);
+  uint64_t rough_quotient = checkpoint & ~0xFFFFFFFFULL; // equal to checkpoint mod 2^32
+  uint64_t candidate = rough_quotient + remainder;
 
-  uint64_t diff = ( checkpoint > candidate ) ? checkpoint - candidate : candidate - checkpoint;
+  uint64_t candidate_diff = ( checkpoint > candidate ) ? checkpoint - candidate : candidate - checkpoint;
   uint64_t best = candidate;
 
   for ( int shift : { -1, 1 } ) {
     uint64_t alter = candidate + ( shift * ( 1ULL << 32 ) );
     uint64_t alter_diff = ( checkpoint > alter ) ? checkpoint - alter : alter - checkpoint;
-    if ( alter_diff < diff ) {
+    if ( alter_diff < candidate_diff ) {
       best = alter;
-      diff = alter_diff;
+      candidate_diff = alter_diff;
     }
   }
 
